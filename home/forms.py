fd391c2523786eadc2c8aca24f31b358f28e7bb7
Kaan <Kaan_K193@hotmail.com>
2019-12-09 18:01:04 +0100

    Update forms

--- home/forms.py
+++ home/forms.py
@@ -3,18 +3,44 @@ from django import forms
 
 
 class CostForm(Form):
-    min_budget = forms.DecimalField(widget=forms.TextInput(attrs={
+    error_css_class = 'uk-form-danger'
+    min_budget = forms.DecimalField(label='Min Budget', required=False, widget=forms.TextInput(attrs={
         'class': 'uk-input',
         'placeholder': 'Enter minimum budget range'
     }))
-    max_budget = forms.DecimalField(widget=forms.TextInput(attrs={
+    max_budget = forms.DecimalField(label='Max Budget', required=False, widget=forms.TextInput(attrs={
         'class': 'uk-input',
         'placeholder': 'Enter maximum budget range'
     }))
+    min_temperature = forms.DecimalField(label='Max Budget', required=False, widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': 'Enter minimum temperature'
+    }))
+    max_temperature = forms.DecimalField(label='Max Budget', required=False, widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': 'Enter maximum temperature'
+    }))
+    postcode = forms.DecimalField(label='Max Budget', required=False, widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': 'Enter your postcode'
+    }))
+    price_per_watt = forms.DecimalField(label='Price/Watt', required=False, widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': 'Enter desired price per watt'
+    }))
+    land_area = forms.DecimalField(label='Land Area', required=False, widget=forms.TextInput(attrs={
+        'class': 'uk-input',
+        'placeholder': 'Enter area of the land'
+    }))
     materials = forms.ChoiceField(choices=[(1, 'Material 1'), (2, 'Material 2'), (3, 'Material 3')],
-                                  widget=forms.Select(attrs={'class': 'uk-select'}))
-    electricity = forms.DecimalField(widget=forms.TextInput(attrs={
+                                  widget=forms.Select(attrs={'class': 'uk-select'}), required=False)
+    electricity = forms.DecimalField(required=False, widget=forms.TextInput(attrs={
         'class': 'uk-input',
         'placeholder': 'Enter your electricity need'
     }))
-
+    '''
+    slider = forms.IntegerField(
+        widget=forms.NumberInput(
+            attrs={'type': 'range', 'step': '1', 'min': '1', 'max': '148', 'class': 'uk-range'}),
+        required=False)
+    '''
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, FormView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from .forms import CostForm
 
 
@@ -11,5 +12,14 @@ class IndexView(SuccessMessageMixin, FormView):
 
     def form_valid(self, form):
         # From here the form is validated and we can do rule chaining etc
-        form.add_error(None, "Submission Failed")
-        return self.form_invalid(form)
+        min_budget = email = form.cleaned_data['min_budget']
+        max_budget = email = form.cleaned_data['max_budget']
+
+        if min_budget > max_budget:
+            form.add_error(None, "Submission Failed: Minimum budget cannot be higher than maximum budget.")
+            return self.form_invalid(form)
+        elif min_budget == max_budget:
+            form.add_error(None, "Submission Failed: Minimum budget cannot be equal to maximum budget.")
+            return self.form_invalid(form)
+
+        return super().form_valid(form)
