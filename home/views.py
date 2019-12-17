c9f8dd8376568a795ad65faed7caad0cee85e0d8
Igor Pejic <igor.pejic@dr.com>
2019-12-17 20:38:40 +0100

    make commercial form work

--- home/views.py
+++ home/views.py
@@ -21,6 +21,39 @@ class IndexView(TemplateView):
     template_name = 'home.html'
 
 
+def clean_form_common(form):
+    logger.info('Receiving requirements form user')
+    max_budget = form.cleaned_data['max_budget']
+    electricity = form.cleaned_data['electricity']
+
+
+    logger.info('Validating that hard requirements (budget and electricity) are present.')
+    if not max_budget and not electricity:
+        form.add_error(None, "Submission Failed: You have to supply more info")
+        return None
+    logger.info('Hard requirements are present and validated.')
+
+    if max_budget:
+        form.cleaned_data['max_budget'] = int(form.cleaned_data['max_budget'])
+
+    logger.info(f'Max budget requirement set to {max_budget}')
+    if form.cleaned_data['min_temperature']:
+        form.cleaned_data['min_temperature'] = int(form.cleaned_data['min_temperature'])
+        logger.info(f'Min temperature requirement set to {form.cleaned_data["min_temperature"]}')
+    if form.cleaned_data['max_temperature']:
+        form.cleaned_data['max_temperature'] = int(form.cleaned_data['max_temperature'])
+        logger.info(f'Max temperature requirement set to {form.cleaned_data["max_temperature"]}')
+
+    if form.cleaned_data['grid_type']:
+        types = ['on-grid', 'off-grid']
+        form.cleaned_data['grid_type'] = types[int(form.cleaned_data['grid_type'])]
+        logger.info(f'Grid type requirement set to {form.cleaned_data["grid_type"]}')
+
+    logger.info('Processing user requirements into hard and soft requirements.')
+    return form
+
+
+
 class UserFormView(SuccessMessageMixin, FormView):
     """View for handling collection of user requirements
     
@@ -40,35 +73,9 @@ class UserFormView(SuccessMessageMixin, FormView):
         :param form: data coming from the form in raw format
         """
         # From here the form is validated and we can do rule chaining etc
-        logger.info('Receiving requirements form user')
-        max_budget = form.cleaned_data['max_budget']
-        electricity = form.cleaned_data['electricity']
-
-
-        logger.info('Validating that hard requirements (budget and electricity) are present.')
-        if not max_budget and not electricity:
-            form.add_error(None, "Submission Failed: You have to supply more info")
+        form = clean_form_common(form)
+        if form is None:
             return self.form_invalid(form)
-        logger.info('Hard requirements are present and validated.')
-
-        if max_budget:
-            form.cleaned_data['max_budget'] = int(form.cleaned_data['max_budget'])
-
-        logger.info(f'Max budget requirement set to {max_budget}')
-        if form.cleaned_data['min_temperature']:
-            form.cleaned_data['min_temperature'] = int(form.cleaned_data['min_temperature'])
-            logger.info('fMin temperature requirement set to {form.cleaned_data["min_temperature"]}')
-        if form.cleaned_data['max_temperature']:
-            form.cleaned_data['max_temperature'] = int(form.cleaned_data['max_temperature'])
-            logger.info(f'Max temperature requirement set to {form.cleaned_data["max_temperature"]}')
-
-        if form.cleaned_data['grid_type']:
-            types = ['on-grid', 'off-grid']
-            form.cleaned_data['grid_type'] = types[int(form.cleaned_data['grid_type'])]
-            logger.info(f'Grid type requirement set to {form.cleaned_data["grid_type"]}')
-
-        logger.info('Processing user requirements into hard and soft requirements.')
-
         user_id = uuid4()
 
         logger.info(f'Starting inference of rules using requiements {form.cleaned_data}')
@@ -100,21 +107,26 @@ class CommercialFormView(SuccessMessageMixin, FormView):
         :param form: data coming from the form in raw format
         """
         # From here the form is validated and we can do rule chaining etc
-        max_budget = form.cleaned_data['max_budget']
-        min_temperature = form.cleaned_data['min_temperature']
-        max_temperature = form.cleaned_data['max_temperature']
-        price_per_watt = form.cleaned_data['price_per_watt']
+
+        form = clean_form_common(form)
+        if form is None:
+            return self.form_invalid(form)
+
         land_area = form.cleaned_data['land_area']
-        electricity = form.cleaned_data['electricity']
         materials = form.cleaned_data['materials']
+        price_per_watt = form.cleaned_data['price_per_watt']
 
-        if not max_budget and not min_temperature and not max_temperature and \
-           not price_per_watt and not land_area and not electricity and not materials:
-
+        if not price_per_watt and not land_area and not electricity and not materials:
             form.add_error(None, "Submission Failed: You have to supply more info")
             return self.form_invalid(form)
 
-        return super().form_valid(form)
+        logger.info(f'Starting inference of rules using requiements {form.cleaned_data}')
+        user_id = uuid4()
+        rule_req = {'uid': str(user_id)}
+        rule_req.update(form.cleaned_data)
+        retres = rule_engine.get_proposal(rule_req)
+        retres['materials'] = ",".join(retres['materials'])
+        return redirect(reverse('solution_view') + "?" + urlencode(retres) + "#solution")
 
 
 class ProjectProposal(TemplateView):
